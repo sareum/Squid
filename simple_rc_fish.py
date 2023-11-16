895de157ba67a37a7769c1e15d5db447e6cd3877
Kai Junge <kai.junge@epfl.ch>
2023-11-16 10:54:20 +0100

    Test changes.

--- simple_rc_fish.py
+++ simple_rc_fish.py
@@ -17,6 +17,8 @@ def main():
     args = parser.parse_args()
 
     offset = 7
+    amp_increment = 0.01
+    offset_increment = 0.01
 
     # Connect gamepad
     gp = Gamepad()
@@ -32,10 +34,12 @@ def main():
     dynamixel.set_operating_mode("position", dyn_id)
 
     # Sine wave properties
-    amp_angle = 20
+    amp_angle = 0
     period = 3
     num_cycles = 10
     #num_cycles = 10 / period
+
+    sequence = 1
 
     # Pump state
     pump_state = [0,0]
@@ -43,23 +47,60 @@ def main():
     timer = time.time()
     while 1:
         # Do something with gamepad
+
+        # LED toggle to see if it's working
         if gp.button_data["circle"] == 1:
             dynamixel.write_to_address(1, 1, 65, ID=dyn_id)
         else:
             dynamixel.write_to_address(0, 1, 65, ID=dyn_id)
 
+        # Change sequence
+        if gp.button_data["up"] == 1:
+            sequence = 1
+        elif gp.button_data["right"] == 1:
+            sequence = 2
+        elif gp.button_data["down"] == 1:
+            sequence = 3
+        elif gp.button_data["left"] == 1:
+            sequence = 4
+        
+        # Change amplitude
+        amp_joystick = gp.axis_data["Ry"]
+        if abs(amp_joystick) > 0.2:
+            # Amp increase
+            if amp_joystick > 0:
+                if amp_angle < 20:
+                    amp_angle += amp_joystick * amp_increment
+                
+            else:
+                if amp_angle > 0:
+                    amp_angle -= amp_joystick * amp_increment
+
+        # Change offset
+        offset_joystick = gp.axis_data["Lx"]
+        if abs(offset_joystick) > 0.2:
+            # Amp increase
+            if offset_joystick > 0:
+                if offset < 14:
+                    offset += offset_joystick * offset_increment
+                
+            else:
+                if offset > 0:
+                    offset -= offset_joystick * offset_increment
+
+        print(sequence, round(amp_angle,2), round(offset,2))
         # t = time.time() - timer
         # period_timer = t%period
 
-        # if args.seq == 1:
+        # if sequence == 1:
         #     # Sequence 1
         #     pump_state = [0,0]
 
-        # elif args.seq == 2:
+        # elif sequence == 2:
         #     # Sequence 2
         #     pump_state = [1,1]
 
-        # elif args.seq == 3:
+        # elif sequence == 3:
         #     # Sequence 3
         #     if period_timer < period/4:
         #         pump_state = [1,1]
@@ -68,7 +109,7 @@ def main():
         #     else:
         #         pump_state = [0,0]
 
-        # elif args.seq == 4:
+        # elif sequence == 4:
         #     # Sequence 4
         #     if period_timer > period/4 and period_timer < period/2:
         #         pump_state = [1,1]
@@ -82,9 +123,8 @@ def main():
         # demand_angle_dynamixel_units = (demand_angle * 4096 / 360) + 2048 + 4096 * (offset/ 360)
         # dynamixel.write_position(demand_angle_dynamixel_units, dyn_id)
 
-        # Send message to arduino
+        # # Send message to arduino
         # arduino.send_message(pump_state)
-
 
         time.sleep(0.001)
         
