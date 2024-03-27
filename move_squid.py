8eda3cf69b58cde45135b781932d76427cc6a354
AlexandreVincent1999 <alexandre.vincent@epfl.ch>
2024-03-27 09:06:09 +0100

    ok

--- move_squid.py
+++ move_squid.py
@@ -1,6 +1,5 @@
 import time
 import numpy as np
-import pygame
 #from plot_module import show_graph
 from dynamixel_controller import Dynamixel
 from time import sleep
@@ -12,12 +11,50 @@ def set_position(time, a, c, T) :
 def write_position(q_dynamixel, IDs) :
     servo.write_position(q_dynamixel, ID=IDs)
 
-# Initialize Pygame
-pygame.init()
+def go_forward(time) :
+    IDs = [1,2,3,4]
+    a = 45
+    c = 180
+    T = 1
+    a_dyna = a * 2048/180
+    c_dyna = c * 2048/180 
+    q_dynamixel = set_position(time, a_dyna, c_dyna, T)
+    write_position(q_dynamixel, IDs)
 
+def go_reverse(time) :
+    IDs = [1,2,3,4]
+    a = 27
+    c = 63
+    T = 1
+    a_dyna = a * 2048/180
+    c_dyna = c * 2048/180 
+    q_dynamixel = set_position(time, a_dyna, c_dyna, T)
+    write_position(q_dynamixel, IDs)
+
+def go_right(time) : 
+    IDs = [3,4]
+    a = 45
+    c = 180
+    T = 1
+    a_dyna = a * 2048/180
+    c_dyna = c * 2048/180 
+    q_dynamixel = set_position(time, a_dyna, c_dyna, T)
+    write_position(q_dynamixel, IDs)
+    write_position(2048, [1,2])
+
+def go_left(time) : 
+    IDs = [1,2]
+    a = 45
+    c = 180
+    T = 1
+    a_dyna = a * 2048/180
+    c_dyna = c * 2048/180 
+    q_dynamixel = set_position(time, a_dyna, c_dyna, T)
+    write_position(q_dynamixel, IDs)
+    write_position(2048, [3,4])
+    
 servo = Dynamixel(ID=[1,2,3,4], descriptive_device_name="XW430-T200R test motor", 
-                    series_name=["xm","xm","xm","xm"], baudrate=3000000, port_name="/dev/ttyUSB0")
-                    #series_name=["xm","xm","xm","xm"], baudrate=3000000, port_name="/dev/tty.usbserial-FT78LT9E")
+                    series_name=["xm","xm","xm","xm"], baudrate=3000000, port_name="/dev/tty.usbserial-FT78LT9E")
 
 servo.begin_communication()
 
@@ -26,73 +63,38 @@ servo.set_operating_mode("position", ID = "all")
 write_position(2048, [1,2,3,4]) #180°
 sleep(1)
 
-i = 0
-num_cycles = 10
-IDs = [1,2,3,4]
-T = 1 #Period
-
-# Real values
-a = 45 #Amplitude en degré
-c = 180 # Center position
-
-# Dynamixel values
-a_dyna = a * 2048/180
-c_dyna = c * 2048/180 
-
-#read_position = np.empty(1000)
-#read_velocity = np.empty(1000)
-#function_value = np.empty(1000)
-#error = np.empty(1000)
+'''read_position = np.empty(1000)
+read_velocity = np.empty(1000)
+function_value = np.empty(1000)
+error = np.empty(1000)'''
 
 timer = time.time()
 
-# Set up the display
-screen = pygame.display.set_mode((640, 480))
+while True :
 
-running = True
+    user_input = input("Appuyez sur 'q' pour quitter : ")
 
-while running :
+    if user_input == 'q':
+        break
 
     t = time.time() - timer
 
-    q_dynamixel = int(set_position(t,a_dyna, c_dyna, T))
-    write_position(q_dynamixel, IDs)
+    go_forward(t)
 
-    #function_value[i] = q   
-    #read_position[i] = servo.read_position(4)
-    #read_velocity[i] = servo.read_velocity(4)
-    #error[i] = 180*(function_value[i] - read_position[i])/position_center
+    '''function_value[i] = q   
+    read_position[i] = servo.read_position(4)
+    read_velocity[i] = servo.read_velocity(4)
+    error[i] = 180*(function_value[i] - read_position[i])/position_center'''
 
-    if t > T*num_cycles :
-        a = 27
-        c = 63
-        T = 1
-        a_dyna = a * 2048/180
-        c_dyna = c * 2048/180
+    if t > 10 :
+        go_reverse(t)
 
+    if t > 20 :
+        go_left(t)
 
-    if t > 2*T*num_cycles :
-        write_position(2048, [1,2]) #180°
-        a = 45
-        c = 180
-        T = 1
-        a_dyna = a * 2048/180
-        c_dyna = c * 2048/180
-        IDs = [3,4]
-
-    # Event handling
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
-
-
-#show_graph([read_position, function_value] , time, ["real values", "function values"]) 
-#show_graph([error] , time, ["Error in degree"])
-#show_graph([read_velocity] , time, ["Velocity"])
+'''show_graph([read_position, function_value] , time, ["real values", "function values"]) 
+show_graph([error] , time, ["Error in degree"])
+show_graph([read_velocity] , time, ["Velocity"])'''
 
 servo.end_communication()
-pygame.quit()
 
