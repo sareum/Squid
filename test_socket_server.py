addd614a552fa52a0ed0395098ba3bef4322fb46
AlexandreVincent1999 <alexandre.vincent@epfl.ch>
2024-03-28 11:56:10 +0100

    ok

--- test_socket_server.py
+++ test_socket_server.py
@@ -94,38 +94,40 @@ error = np.empty(1000)'''
 
 timer = time.time()
 
-while True :
-
-    # Check for incoming connections
-    readable, _, _ = select.select([server_socket], [], [], 0.1)
-    for sock in readable:
-        # Accept the connection
-        connection, client_address = server_socket.accept()
-        connection.setblocking(False)
-        print("Connection from", client_address)
-
-    t = time.time() - timer
-
+while True:
     try:
-        # Receive data from the client
-        while True:
+        # Check for incoming connections
+        readable, _, _ = select.select([server_socket], [], [], 0.1)
+        for sock in readable:
+            # Accept the connection
+            connection, client_address = server_socket.accept()
+            connection.setblocking(False)
+            print("Connection from", client_address)
+
             try:
-                json_data = connection.recv(1024).decode()  # Receive data
-                if not json_data:
-                    break
-                data = json.loads(json_data)  # Deserialize JSON data
-                print("Received:", data)
-                
-            except json.JSONDecodeError:
-                print("Invalid JSON received")
-
-    except socket.error:
+                # Receive data from the client
+                while True:
+                    try:
+                        json_data = connection.recv(1024).decode()  # Receive data
+                        if not json_data:
+                            break
+                        data = json.loads(json_data)  # Deserialize JSON data
+                        print("Received:", data)
+                    except json.JSONDecodeError:
+                        print("Invalid JSON received")
+            except socket.error:
                 pass  # No data available
+            finally:
+                # Clean up the connection
+                connection.close()
 
-    finally:
-        connection.close() # Clean up the connection
+        # Execute go_forward function
+        t = time.time() - timer
+        go_forward(t)
 
-    t = time.time() - timer
-    go_forward(t)
+    except BlockingIOError:
+        pass  # No incoming connections available
 
+# Close the server socket
+server_socket.close()
 servo.end_communication()
