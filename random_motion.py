d583196133cbb12ef1d9b52fdad3b7c74fd0c855
AlexandreVincent1999 <alexandre.vincent@epfl.ch>
2024-04-22 09:24:52 +0200

    Update random_motion.py

--- random_motion.py
+++ random_motion.py
@@ -18,7 +18,7 @@ def go_forward(time) :
     IDs = [1,2,3,4]
     a = 45
     c = 180
-    T = 1
+    T = 1.6
     a_dyna = a * 2048/180
     c_dyna = c * 2048/180 
     q_dynamixel = set_position(time, a_dyna, c_dyna, T)
@@ -28,7 +28,7 @@ def go_reverse(time) :
     IDs = [1,2,3,4]
     a = 27
     c = 63
-    T = 0.5
+    T = 1
     a_dyna = a * 2048/180
     c_dyna = c * 2048/180 
     q_dynamixel = set_position(time, a_dyna, c_dyna, T)
