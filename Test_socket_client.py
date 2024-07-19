f2ac6997d101fbda12f0e581b8658cb993d1efca
gabbianc01 <gabriele.bianchi@epfl.ch>
2024-07-19 11:07:44 +0200

    Update Test_socket_client.py

--- Test_socket_client.py
+++ Test_socket_client.py
@@ -63,6 +63,7 @@ def write_motor_position_sin(time, a_right, c_right, T_right, a_left, c_left, T_
     servo.write_position(q_dynamixel_right, ID_right)
     q_dynamixel_left = sin_position(time, a_dyna_left, c_dyna_left, T_left)
     servo.write_position(q_dynamixel_left, ID_left)
+    
     return q_dynamixel_right, q_dynamixel_left 
 
 servo.begin_communication()
@@ -94,10 +95,7 @@ while True :
     
     State = data.get("State")
      
-    motor_command = 180*write_motor_position_sin(t, a_right, c_right, T_right, a_left, c_left, T_left)/2048
-    
-    motor_command_right = motor_command[0]
-    motor_command_left = motor_command[1]
+    motor_command_right, motor_command_left = 180*write_motor_position_sin(t, a_right, c_right, T_right, a_left, c_left, T_left)/2048
 
     read_position_right = 180*servo.read_position(1)/2048
     read_position_left = 180*servo.read_position(3)/2048
