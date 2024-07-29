da1eff2c347d38ddd33ddb37c2b909fabc1f6755
gabbianc01 <gabriele.bianchi@epfl.ch>
2024-07-29 17:29:06 +0200

    Update Test_socket_client.py

--- Test_socket_client.py
+++ Test_socket_client.py
@@ -184,7 +184,7 @@ while True :
         "Motor_position_left" : read_position_left, 
         "Motor_command_right" : motor_command_right, 
         "Motor_command_left" : motor_command_left,
-        "Time": time_values
+        "Time": time_values,
     })
     
     if t >20:
