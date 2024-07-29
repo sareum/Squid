31be357baf8b96de2ffc995c77f4233fca4ebfc3
gabbianc01 <gabriele.bianchi@epfl.ch>
2024-07-29 18:05:34 +0200

    Update Test_socket_client.py

--- Test_socket_client.py
+++ Test_socket_client.py
@@ -155,13 +155,13 @@ while True :
     c_left = 180
     T_left = 1
     motor_command,t_mod = write_motor_position_triangle(t, a_right, c_right, T_right, 0.2, 0.8, a_left, c_left, T_left, 0.2, 0.8)
-    '''print("t_mod: ",t_mod,"time: ", t)
+    print("t_mod: ",t_mod,"time: ", t)
     if t_mod >0.75 and t_mod< 0.85:
         message = 'ready'
         message_json = json.dumps(message)
         client_socket.send(message_json.encode())
         print('request sent')
-        time.sleep(0.1)'''
+        time.sleep(0.1)
         
     motor_command_right = 180*motor_command[0]/2048
     motor_command_left = 180*motor_command[1]/2048
