6c900626b6bd1536975c3a6c3dd9c3b5f21a1823
gabbianc01 <gabriele.bianchi@epfl.ch>
2024-07-29 11:59:30 +0200

    Update Test_socket_client.py

--- Test_socket_client.py
+++ Test_socket_client.py
@@ -122,16 +122,16 @@ while True :
     
     if State == 0  :
         break
-message = 'start'
+'''message = 'start'
 message_json = json.dumps(message)
 client_socket.send(message_json.encode())
-time.sleep(2)
+time.sleep(2)'''
 print('starting to send data...')
 for i in range(len(data_to_send)):
     print('sending the',i,'batch...')
     json_position = json.dumps(data_to_send[i])
     client_socket.send(json_position.encode())
-    time.sleep(0.3)
+    time.sleep(5)
 
 message = 'end'
 message_json = json.dumps(message)
