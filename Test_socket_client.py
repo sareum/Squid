aacfc1dbaac556002cd0643c1de4a7d357e7a118
gabbianc01 <gabriele.bianchi@epfl.ch>
2024-07-30 15:16:50 +0200

    Update Test_socket_client.py

--- Test_socket_client.py
+++ Test_socket_client.py
@@ -175,6 +175,7 @@ while True :
 
     motor_command,t_mod = write_motor_position_triangle(t, a_right, c_right, T, opening_ratio, closing_ration, a_left, c_left, T, opening_ratio, closing_ration)
     # check if a period T has expired:
+    print(t%T)
     if t%T>0.90: 
         message = 'r'
         message_json = json.dumps(message)
