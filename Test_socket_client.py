433a53282430d324b92d9eceb16e7d1785721ac6
gabbianc01 <gabriele.bianchi@epfl.ch>
2024-07-30 12:40:56 +0200

    Update Test_socket_client.py

--- Test_socket_client.py
+++ Test_socket_client.py
@@ -45,7 +45,7 @@ def sin_position(time, a, c, T) :
 
 def triangle_wave_position(t, a, T, rise_time_ratio, fall_time_ratio):
     period = T
-    peak_value = 210  # Valore massimo fisso
+    peak_value = 220  # Valore massimo fisso
     valley_value = peak_value - a  # Valore minimo variabile in base all'ampiezza
 
     rise_time = rise_time_ratio * period
