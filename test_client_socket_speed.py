0df4c3feb7c22165e7844f54101078fcbbbe9f2f
gabbianc01 <gabriele.bianchi@epfl.ch>
2024-08-16 11:22:34 +0200

    Update test_client_socket_speed.py

--- test_client_socket_speed.py
+++ test_client_socket_speed.py
@@ -209,8 +209,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     break
                 its_opening = False
             
-            motor_command[0] = f"{motor_command[0]:.4g}"
-            motor_command[1] = f"{motor_command[1]:.4g}"
+            motor_command[0] = float(f"{motor_command[0]:.4g}")
+            motor_command[1] = float(f"{motor_command[1]:.4g}")
             
             data_to_encode = str(motor_command)+str(serial_reads)
             #encode the data in utf-8 for socket comunication
