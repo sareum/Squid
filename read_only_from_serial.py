787c25e1f844ba5112467c97c5ae358abc894368
gabbianc01 <gabriele.bianchi@epfl.ch>
2024-08-20 14:36:57 +0200

    Update read_only_from_serial.py

--- read_only_from_serial.py
+++ read_only_from_serial.py
@@ -1,12 +1,12 @@
 import serial
-import time
+
 
 serial_port = '/dev/ttyACM0'  # Cambia questo con la tua porta
 baud_rate = 115200  # Questo deve corrispondere al baud rate impostato nel Teensy
 ser = serial.Serial(serial_port, baud_rate, timeout=1)
-ser.close()
+#ser.close()
 
-ser = serial.Serial(serial_port,baud_rate,timeout= 1)
+#ser = serial.Serial(serial_port,baud_rate,timeout= 1)
 #ser.open()
 ser.reset_input_buffer()
 ser.reset_output_buffer()
@@ -15,7 +15,9 @@ print(f"Connessione aperta sulla porta {serial_port} con baud rate {baud_rate}")
 while True:
     if ser.in_waiting > 0:
         # Legge una riga di dati dalla seriale
-        data = ser.read(ser.in_waiting)
-        serial_reads = data.decode('utf-8').rstrip()
+        
+        serial_reads = ser.readline().decode('utf-8').rstrip()
         print("dati: ",serial_reads)
-        time.sleep(0.01)
+        ser.reset_input_buffer()
+        ser.reset_output_buffer()
+
