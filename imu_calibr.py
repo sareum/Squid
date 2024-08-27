0a9c9e27cc33e810fcc7e0747b408aa208441479
gabbianc01 <gabriele.bianchi@epfl.ch>
2024-08-27 14:42:38 +0200

    Update imu_calibr.py

--- imu_calibr.py
+++ imu_calibr.py
@@ -57,6 +57,7 @@ def correction(data):
 
 def main():
     try:
+        q0 = np.zeros(1,4)
         data1, data2 = read_sensors()
         data1 = correction(data1)
         data2 = correction(data2)
@@ -69,9 +70,9 @@ def main():
         gyr_data2 = [data2[3],data2[4],data2[5]]
         mag_data2 = [data2[0],data2[1],data2[2]]
         #get the first readings
-        print(type(np.array(gyr_data1)))
-        ekf1 = ahrs.filters.ekf.EKF(gyr=np.array(gyr_data1), acc=np.array(acc_data1), mag=np.array(mag_data1), frequency=10.0)
-        ekf2 = ahrs.filters.ekf.EKF(gyr=np.array(gyr_data2), acc=np.array(acc_data2), mag=np.array(mag_data2), frequency=10.0)
+        
+        ekf1 = ahrs.filters.ekf.EKF(q0 = q0,gyr=np.array(gyr_data1), acc=np.array(acc_data1), mag=np.array(mag_data1), frequency=10.0)
+        ekf2 = ahrs.filters.ekf.EKF(q0 = q0,gyr=np.array(gyr_data2), acc=np.array(acc_data2), mag=np.array(mag_data2), frequency=10.0)
 
         q0_1 = ekf1.Q
         q0_2 = ekf2.Q
