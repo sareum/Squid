aa5822947008afcc5b0d24a26599a8ad2e981945
gabbianc01 <gabriele.bianchi@epfl.ch>
2024-08-27 15:46:08 +0200

    Update imu_calibr.py

--- imu_calibr.py
+++ imu_calibr.py
@@ -73,8 +73,8 @@ def main():
         mag_data2 = [[data2[0]],[data2[1]],[data2[2]]]
         q02 = ahrs.common.orientation.acc2q(np.array(acc_data2))
         #get the first readings
-        ekf1 = ahrs.filters.ekf.EKF(gyr=np.array(gyr_data1), acc=np.array(acc_data1), mag=np.array(mag_data1), frequency=10.0,frame = 'NED',q0 = q01)
-        ekf2 = ahrs.filters.ekf.EKF(gyr=np.array(gyr_data2), acc=np.array(acc_data2), mag=np.array(mag_data2), frequency=10.0,frame = 'NED',q0 = q02)
+        ekf1 = ahrs.filters.ekf.EKF(gyr=np.array(gyr_data1), acc=np.array(acc_data1), mag=np.array(mag_data1), frequency=10.0,frame = 'NED',q0 =[0.7071, 0.0, -0.7071, 0.0])
+        ekf2 = ahrs.filters.ekf.EKF(gyr=np.array(gyr_data2), acc=np.array(acc_data2), mag=np.array(mag_data2), frequency=10.0,frame = 'NED',q0 = [0.7071, 0.0, -0.7071, 0.0])
 
         q0_1 = ekf1.Q/np.linalg.norm(ekf1.Q)
         q0_2 = ekf2.Q/np.linalg.norm(ekf2.Q)
