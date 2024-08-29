8091c8b827fca7b8af4ea3597a28ca3a9fcb206b
gabbianc01 <gabriele.bianchi@epfl.ch>
2024-08-29 17:31:15 +0200

    aaaa

---
+++ aaaaaaaaa.py
@@ -0,0 +1,19 @@
+import time
+# Imposta l'indirizzo I2C del Teensy
+I2C_ADDRESS = 0x08
+# Inizializza il bus I2C (bus 1 per Raspberry Pi 3 e successivi)
+bus = smbus.SMBus(1)
+def read_data():
+    try:
+        # Leggi una sequenza di 16 byte dal dispositivo I2C
+        data = bus.read_i2c_block_data(I2C_ADDRESS, 0, 16)
+        # Converti i byte in stringa (assumendo che il Teensy invii dati ASCII)
+        return ''.join(chr(i) for i in data)
+    except OSError as e:
+        print(f"Errore durante la lettura: {e}")
+        return None
+while True:
+    received_data = read_data()
+    if received_data:
+        print(f"Ricevuto: {received_data}")
+    time.sleep(1) # Aspetta un secondo prima di leggere di nuovo
