4ca20e6bc9fb5cd5ce683bbd371b0f39ab4041e9
gabbianc01 <gabriele.bianchi@epfl.ch>
2024-08-30 09:47:22 +0200

    Update aaaaaaaaa.py

--- aaaaaaaaa.py
+++ aaaaaaaaa.py
@@ -1,22 +1,33 @@
+import smbus2
+import struct
 import time
-import smbus
-# Imposta l'indirizzo I2C del Teensy
-I2C_ADDRESS = 0x08
-# Inizializza il bus I2C (bus 1 per Raspberry Pi 3 e successivi)
-bus = smbus.SMBus(1)
-def read_data():
-    try:
-        # Leggi una sequenza di 16 byte dal dispositivo I2C
-        data = bus.read_i2c_block_data(I2C_ADDRESS, 0, 16)
-        # Converti i byte in stringa (assumendo che il Teensy invii dati ASCII)
-        return ''.join(chr(i) for i in data)
-    except OSError as e:
-        print(f"Errore durante la lettura: {e}")
-        return None
-while True:
-    tic = time.time()
-    received_data = read_data()
-    if received_data:
-        print(f"Ricevuto: {received_data}")
-    print(time.time()-tic)
-    #time.sleep(1) # Aspetta un secondo prima di leggere di nuovo
+# Indirizzo I2C del Teensy
+TEENSY_I2C_ADDRESS = 0x08
+# Creazione dell'oggetto bus
+bus = smbus2.SMBus(1)
+def read_quaternions():
+    # Lettura di 32 byte (8 float a 4 byte ciascuno)
+    data = bus.read_i2c_block_data(TEENSY_I2C_ADDRESS, 0, 32)
+    # Conversione dei dati binari in float
+    qW1, qX1, qY1, qZ1, qW2, qX2, qY2, qZ2 = struct.unpack('f' * 8, bytearray(data))
+    return qW1, qX1, qY1, qZ1, qW2, qX2, qY2, qZ2
+def main():
+    while True:
+        try:
+            # Legge i dati quaternion dal Teensy
+            quaternions = read_quaternions()
+            print(f"qW1: {quaternions[0]}, qX1: {quaternions[1]}, qY1: {quaternions[2]}, qZ1: {quaternions[3]}")
+            print(f"qW2: {quaternions[4]}, qX2: {quaternions[5]}, qY2: {quaternions[6]}, qZ2: {quaternions[7]}")
+            print("-" * 50)
+            time.sleep(0.1)
+        except OSError as e:
+            print(f"Errore di comunicazione I2C: {e}")
+            time.sleep(1)
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
