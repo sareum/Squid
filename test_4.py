a21dba9f2eb3dd72e2b6c6e827eeb039381b4e36
sareum <54650931+sareum@users.noreply.github.com>
2024-10-07 10:23:50 +0200

    Add files via upload

--- test_4.py
+++ test_4.py
@@ -79,9 +79,9 @@ a_right = 75
 a_left = 75
 T_right = 2  # Period for right motors
 T_left = 2   # Period for left motors
-rise_time_ratio_right = 3
-fall_time_ratio_right = 1
-rise_time_ratio_left = 3
+rise_time_ratio_right = 3 #moving inward - thrust stroke
+fall_time_ratio_right = 1 # moving outward - return stroke
+rise_time_ratio_left = 1
 fall_time_ratio_left = 1
 
 print(f"End motor setup")
