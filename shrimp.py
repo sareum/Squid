0c8f0dd307a124f84767a8deabcf29ed7bdb3d28
sareum <54650931+sareum@users.noreply.github.com>
2025-06-06 13:21:10 +0200

    Add files via upload

--- shrimp.py
+++ shrimp.py
@@ -61,7 +61,7 @@ def write_motor_position_triangle(t, a_right, T_right, rise_time_ratio_right, fa
 
     q_dynamixel_right, t_mod_right = triangle_wave_position(t_right, a_right, T_right, rise_time_ratio_right, fall_time_ratio_right)
     q_dynamixel_left, t_mod_left = triangle_wave_position(t_left, a_left, T_left, rise_time_ratio_left, fall_time_ratio_left)
-    q_dynamixel_left = 340 - q_dynamixel_left
+    q_dynamixel_left = 360 - q_dynamixel_left
     ####################################
 
     position_motor_step_right = q_dynamixel_right * 2048 / 180
@@ -84,8 +84,8 @@ servo = Dynamixel(
 servo.begin_communication()
 servo.set_operating_mode("position", ID="all")
 
-a_right =120
-a_left = 120
+a_right =80
+a_left = 80
 T_right = 2
 T_left = 2
 rise_time_ratio_right = 0.2
